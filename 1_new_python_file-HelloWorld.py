97fa65f56d951ba98e3a64c0ceeff7bd4d2d51a8
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-08-01 22:39:05 -0300

    Update 1_new_python_file-HelloWorld.py

--- 1_new_python_file-HelloWorld.py
+++ 1_new_python_file-HelloWorld.py
@@ -5,6 +5,7 @@
 #Hello World em python
 
 print ("Hello World em Python")
+print ("começando em python")
 
 # os comandos em python são case sensitive, isso significa que as letras maiusculas e minusculas são levadas em consideração na hora de
 #executar o comando, se escrevermos esse comando em letra maiuscula irá retornar um erro como
