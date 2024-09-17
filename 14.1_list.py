3a7485964a42defcb25fc379697840b0224ba60c
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-09-17 00:18:32 -0300

    Add files via upload

---
+++ 14.1_list.py
@@ -0,0 +1,11 @@
+list_numeric = [5,10,20,40]
+
+print(list_numeric)
+
+#adicionando um número na lista
+
+add_numero_lista = int(input("Digite o número para ser adicionado na lista: "))
+
+list_numeric.append(add_numero_lista)
+
+print(list_numeric)
