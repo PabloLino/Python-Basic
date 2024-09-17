803d0b2bcf644f2f873ae41c8d1d9687de98aa88
Pablo Lino <118943316+PabloLino@users.noreply.github.com>
2024-09-17 00:20:13 -0300

    Add files via upload

---
+++ 15_indice_string.py
@@ -0,0 +1,11 @@
+Var_string = "Banco de dados"
+
+print("primeiro caractere:",Var_string[0])
+
+print("terceiro caractere:",Var_string[2])
+
+print("quinto caractere(vazio no caso, é o espaço em branco):",Var_string[5])
+
+print("Último caractere:",Var_string[13])
+
+print("Aqui está um caractere inexistente na variável 'Banco de dados':", Var_string[14])
